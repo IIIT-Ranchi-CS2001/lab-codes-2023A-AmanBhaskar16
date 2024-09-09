9e92c4ab09a5f38e85d578437788c055e5e631a5
AmanBhaskar16 <141729237+AmanBhaskar16@users.noreply.github.com>
2024-09-09 20:26:58 +0530

    Update program3.py

--- program3.py
+++ program3.py
@@ -1,5 +1,5 @@
-temp = float(input("Enter temperature in celcius : "));
+temp = float(input("Enter temperature in celcius : "))
 print("The temperature in fahrenheit : ")
-x = temp*1.8+32;
-y = format(x, ".2f");
-print(y);
+x = temp*1.8+32
+y = format(x, ".2f")
+print(y)
